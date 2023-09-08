1a217e6debabf271f31a6a9651c6e89030f29c42
Anastasiia211 <nastik121@ukr.net>
2023-09-08 03:26:28 +0300

    change file

--- src/js/api.js
+++ src/js/api.js
@@ -21,16 +21,3 @@ const optionsImage = async (searchQuery, perPage, numberPage) => {
 
 export { optionsImage };
     
-// export const BASE_URL = 'https://pixabay.com/api/';
-// export const API_KEY = '39291497-afd0346a938db9581e617b7f9';
-// export const options = {
-//     params: {
-//     key: API_KEY,
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: true,
-//     per_page: 40,
-//     page: 1,
-//     q: '',
-//     },
-// };
